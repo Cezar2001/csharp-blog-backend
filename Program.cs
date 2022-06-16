b43ef081ab52ec5eb7ce794254f2524532814026
Cezar <cezarlantosgmail.com>
2022-06-16 18:01:07 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using csharp_blog_backend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BlogContext>(opt =>
+    opt.UseInMemoryDatabase("posts"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
